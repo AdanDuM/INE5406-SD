93f3a1c4d67dfabacacdb0fb5eb72edf4d852030
Adan Gomes <AdanDuM@users.noreply.github.com>
2016-10-19 19:50:32 -0200

    Update switch2x2.vhd

--- switch2x2.vhd
+++ switch2x2.vhd
@@ -4,13 +4,13 @@ use IEEE.std_logic_1164.all;
 -- Alunos: Adan Pereira Gomes e Wesley Mayk Gama Luz
  
  entity switch2x2 is
-	 generic(width:integer:= 8);
+	 generic(width: integer:= 8);
 	 port(
-		 inpt0: in std_logic_vector(width-1 downto 0);
-		 inpt1: in std_logic_vector(width-1 downto 0);
-		 ctrl: in std_logic_vector(1 downto 0);
-		 outp0: out std_logic_vector(width-1 downto 0);
-		 outp1: out std_logic_vector(width-1 downto 0)
+		 in_0	: in std_logic_vector(width-1 downto 0);
+		 in_1	: in std_logic_vector(width-1 downto 0);
+		 ctrl	: in std_logic_vector(1 downto 0);
+		 out_0: out std_logic_vector(width-1 downto 0);
+		 out_1: out std_logic_vector(width-1 downto 0)
 	 );
  end entity;
  

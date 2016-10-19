eb8a3d6657b9192e602f47e8e1b981002a66713d
Adan Gomes <AdanDuM@users.noreply.github.com>
2016-10-19 19:49:50 -0200

    Update dec7seg.vhd

--- dec7seg.vhd
+++ dec7seg.vhd
@@ -5,23 +5,24 @@ use IEEE.std_logic_1164.all;
 
 entity dec7seg is
 	port (
-		E: in std_logic_vector(4 downto 0);
-		O: out std_logic_vector(6 downto 0)
+		bcd_in: in std_logic_vector(3 downto 0);
+		dec_out: out std_logic_vector(6 downto 0)
 	);
 end entity;
 
 architecture structural of dec7seg is
 begin
 
-	O <=	"1000000" when E = "00000" else -- 0
-			"1111001" when E = "00001" else -- 1
-			"0100100" when E = "00010" else -- 2
-			"0110000" when E = "00011" else -- 3
-			"0011001" when E = "00100" else -- 4
-			"0010010" when E = "00101" else -- 5
-			"0000010" when E = "00110" else -- 6
-			"1111000" when E = "00111" else -- 7
-			"0000000" when E = "01000" else -- 8
-			"0010000" when E = "01001";	  -- 9
+	dec_out <=	"1000000" when bcd_in = "0000" else -- 0
+					"1111001" when bcd_in = "0001" else -- 1
+					"0100100" when bcd_in = "0010" else -- 2
+					"0110000" when bcd_in = "0011" else -- 3
+					"0011001" when bcd_in = "0100" else -- 4
+					"0010010" when bcd_in = "0101" else -- 5
+					"0000010" when bcd_in = "0110" else -- 6
+					"1111000" when bcd_in = "0111" else -- 7
+					"0000000" when bcd_in = "1000" else -- 8
+					"0010000" when bcd_in = "1001" else -- 9
+					"0000110";									-- E - Error
 			
 end architecture;
